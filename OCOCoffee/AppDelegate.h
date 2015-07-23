6b8eb423fd87e4602e146c03ef7e288447b779e6
franky2013 <296699151@qq.com>
2015-07-23 21:33:02 +0800

    Initial Commit

---
+++ OCOCoffee/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCOCoffee
+//
+//  Created by panxiaobing on 15/7/23.
+//  Copyright (c) 2015年 gionee_panxb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OCOCoffee/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  OCOCoffee
+//
+//  Created by panxiaobing on 15/7/23.
+//  Copyright (c) 2015年 gionee_panxb. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
