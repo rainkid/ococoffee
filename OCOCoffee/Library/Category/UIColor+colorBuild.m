52edf8bc64ff2afe9fa4dd3047d374d4a239d79e
franky2013 <296699151@qq.com>
2015-09-08 23:55:10 +0800

    udapte

--- OCOCoffee/Library/Category/UIColor+colorBuild.m
+++ OCOCoffee/Library/Category/UIColor+colorBuild.m
@@ -6,16 +6,50 @@
 //  Copyright (c) 2013年 Gionee. All rights reserved.
 //
 
+#define DEFAULT_VOID_COLOR [UIColor whiteColor]
+
 #import "UIColor+colorBuild.h"
 
 @implementation UIColor (colorBuild)
 
 + (UIColor *)colorFromHexString:(NSString *)hexString {
-    unsigned rgbValue = 0;
-    NSScanner *scanner = [NSScanner scannerWithString:hexString];
-    [scanner setScanLocation:1]; // bypass '#' character
-    [scanner scanHexInt:&rgbValue];
-    return [UIColor colorWithRed:((rgbValue & 0xFF0000) >> 16)/255.0 green:((rgbValue & 0xFF00) >> 8)/255.0 blue:(rgbValue & 0xFF)/255.0 alpha:1.0];
+//    unsigned rgbValue = 0;
+//    NSScanner *scanner = [NSScanner scannerWithString:hexString];
+//    [scanner setScanLocation:1]; // bypass '#' character
+//    [scanner scanHexInt:&rgbValue];
+//    return [UIColor colorWithRed:((rgbValue & 0xFF0000) >> 16)/255.0 green:((rgbValue & 0xFF00) >> 8)/255.0 blue:(rgbValue & 0xFF)/255.0 alpha:1.0];
+    
+    
+    NSString *cString = [[hexString stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] uppercaseString];
+    
+    if ([cString length] < 6)
+        return DEFAULT_VOID_COLOR;
+    if ([cString hasPrefix:@"#"])
+        cString = [cString substringFromIndex:1];
+    if ([cString length] != 6)
+        return DEFAULT_VOID_COLOR;
+    
+    NSRange range;
+    range.location = 0;
+    range.length = 2;
+    NSString *rString = [cString substringWithRange:range];
+    
+    range.location = 2;
+    NSString *gString = [cString substringWithRange:range];
+    
+    range.location = 4;
+    NSString *bString = [cString substringWithRange:range];
+    
+    
+    unsigned int r, g, b;
+    [[NSScanner scannerWithString:rString] scanHexInt:&r];
+    [[NSScanner scannerWithString:gString] scanHexInt:&g];
+    [[NSScanner scannerWithString:bString] scanHexInt:&b];
+    
+    return [UIColor colorWithRed:((float) r / 255.0f)
+                           green:((float) g / 255.0f)
+                            blue:((float) b / 255.0f)
+                           alpha:1.0f];
 }
 
 + (UIColor *)colorFromHexString:(NSString *)hexString andWithAlpha:(CGFloat)alpha{
