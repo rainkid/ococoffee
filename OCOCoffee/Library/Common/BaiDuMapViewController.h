873206c36e23ad55db02417e9ece54e6bdb3c68a
franky2013 <296699151@qq.com>
2015-09-08 23:50:13 +0800

    update

---
+++ OCOCoffee/Library/Common/BaiDuMapViewController.h
@@ -0,0 +1,18 @@
+//
+//  BaiDuMapViewController.h
+//  OCOCoffee
+//
+//  Created by panxiaobing on 15/9/8.
+//  Copyright (c) 2015年 gionee_panxb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaiDuMapViewController : UIViewController
+
+@property(nonatomic,strong) NSString *logitute;
+@property(nonatomic,strong) NSString *latitude;
+@property(nonatomic,strong) NSString *cityName;
+@property(nonatomic,strong) NSString *address;
+
+@end
