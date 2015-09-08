ccc0416925a616cb92c5b9f08057da3cab773ba2
franky2013 <296699151@qq.com>
2015-09-08 23:49:08 +0800

    upate

---
+++ OCOCoffee/Views/Cell/InviteTableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  InviteTableViewCell.h
+//  OCOCoffee
+//
+//  Created by panxiaobing on 15/8/31.
+//  Copyright (c) 2015年 gionee_panxb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InviteTableViewCell : UITableViewCell
+
+
+@property(nonatomic,strong) UILabel *typeLabel;
+@property(nonatomic,strong) UITextField *textfield;
+
+@end
