2124399b10b2a1a3239c1a0b4f5c6725bc15458a
franky2013 <296699151@qq.com>
2015-09-11 01:33:43 +0800

    udpate

--- OCOCoffee/AppDelegate.m
+++ OCOCoffee/AppDelegate.m
@@ -90,6 +90,8 @@
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    
+    [BMKMapView willBackGround];
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
@@ -103,6 +105,8 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    
+    [BMKMapView didForeGround];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
